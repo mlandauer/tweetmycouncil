dd8b930afee19988e3e3e10463f3daf5b37c6927
Henare Degan <henare.degan@gmail.com>
2011-12-03 16:49:12 +1100

    Add more debugging and handle not getting an lga_code response from Geo2Gov

--- app.rb
+++ app.rb
@@ -63,12 +63,13 @@ EM.schedule do
   client.track('#tmyc') do |status|
     puts "Responding to tweet from @#{status.user.screen_name}"
     if status.geo
-      puts "Found geo information"
+      puts "Found geo information: #{status.geo}"
       responses = [
         "Crikey! I can see you",
         "I've found your flamin location"
       ]
-      lga_code = Geo2gov.new(status.geo.coordinates[0], status.geo.coordinates[1]).lga_code
+      geo2gov_response = Geo2gov.new(status.geo.coordinates[0], status.geo.coordinates[1])
+      lga_code = geo2gov_response.lga_code[3..-1] if geo2gov_response.lga_code
       if lga_code
         puts "Found LGA code"
         response = "#{responses.sample}: #{lga_code}"
