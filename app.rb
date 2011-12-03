994fddf8d8274f6065ed3784d582e93f6f029658
Henare Degan <henare.degan@gmail.com>
2011-12-03 13:16:49 +1100

    Respond with an LGA code from Geo2Gov

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 #!/usr/bin/env ruby
 
+$LOAD_PATH.unshift File.dirname(__FILE__) + '/lib'
+
 require 'sinatra'
 require 'tweetstream'
 require 'yaml'
 require 'twitter'
+require 'httparty'
+require 'geo2gov'
 
 if File.exists? 'configuration.yaml'
   configuration = YAML.load_file('configuration.yaml')
@@ -31,20 +35,24 @@ EM.schedule do
 
   client = TweetStream::Client.new
   client.track('#tmyc') do |status|
+    puts "Responding to tweet from @#{status.user.screen_name}"
     if status.geo
+      puts "Found geo information"
       responses = [
         "Crikey! I can see you",
         "I've found your flamin location"
       ]
-      response = "#{responses.sample}: #{status.geo.coordinates}"
+      lga_code = Geo2gov.new(status.geo.coordinates[0], status.geo.coordinates[1]).lga_code
+      if lga_code
+        puts "Found LGA code"
+        response = "#{responses.sample}: #{lga_code}"
+      else
+        response = "#{responses.sample}: #{status.geo.coordinates}"
+      end
     else
-      responses = [
-        "I can't find where the bloody hell you are",
-        "Can't find any location data on your tweet, mate",
-        "Strewth, you need to add location data"
-      ]
-      response = responses.sample
+      response = no_geo_response
     end
+    puts "Responding with \"@#{status.user.screen_name} #{response} (#{rand(500)})\""
     Twitter.update("@#{status.user.screen_name} #{response} (#{rand(500)})", :in_reply_to_status_id => status.id.to_i)
   end
   client.on_error do |message|
@@ -56,3 +64,11 @@ EM.error_handler{ |e|
   puts "Error raised during event loop: #{e.message}"
 }
 
+def no_geo_response
+  responses = [
+    "I can't find where the bloody hell you are",
+    "Can't find any location data on your tweet, mate",
+    "Strewth, you need to add location data"
+  ]
+  response = responses.sample
+end
