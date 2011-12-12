ee4368595f0cd9667e16bc147635fb54231ed3d1
Matthew Landauer <matthew@openaustralia.org>
2011-12-12 14:39:05 +1100

    Switch over to using Mailgun for sending email

--- mailer.rb
+++ mailer.rb
@@ -1,13 +1,13 @@
 require 'action_mailer'
 
-if ENV['SENDGRID_USERNAME'] && ENV['SENDGRID_PASSWORD']
+if ENV['MAILGUN_SMTP_LOGIN'] && ENV['MAILGUN_SMTP_PASSWORD']
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
+    :port           => ENV['MAILGUN_SMTP_PORT'] 
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'tweetmycouncil.heroku.com'
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com'
   }
 else
   # Send to Mailcatcher for development because it rocks!
